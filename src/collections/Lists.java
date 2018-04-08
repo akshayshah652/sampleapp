b07f6579add370aaf55a91fa6ba8e3645f9affff
akshayshah652 <akshaybh@buffalo.edu>
2018-04-08 16:26:30 -0700

    commit on testing branch

--- src/collections/Lists.java
+++ src/collections/Lists.java
@@ -2,6 +2,7 @@ package collections;
 
 public class Lists {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello1");
+		System.out.println("Hello testing");
 	}
 }
